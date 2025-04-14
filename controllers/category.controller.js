751413e339b5b6a3f10982d5ca7a1db048dd39d3
sagunji <karanjit.sagun01@gmail.com>
2025-04-13 23:54:30 -0400

    Sort category via draw numbers

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,11 +1,19 @@
 const drawsData = require("../data/ee-draws.json");
 
 const getCategories = (_, res) => {
-  const categories = [
-    ...new Set(drawsData.draws.map((draw) => draw.category).filter((x) => x)),
-  ];
+  const categoryCounts = drawsData.draws.reduce((acc, draw) => {
+    if (draw.category) {
+      acc[draw.category] = (acc[draw.category] || 0) + 1;
+    }
+    return acc;
+  }, {});
 
-  res.json({ categories });
+  const sortedCategories = Object.entries(categoryCounts)
+    .map(([category, count]) => ({ category, count }))
+    .sort((a, b) => b.count - a.count)
+    .map((item) => item.category);
+
+  res.json({ categories: sortedCategories });
 };
 
 module.exports = {
